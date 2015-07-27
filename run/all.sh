6c7c2baf1e3fdb838f78dc01272f3ce28630ad8c
Martin Aigner <martin@maigner.net>
2015-07-27 13:23:14 +0200

    added new experiments to all.sh

--- run/all.sh
+++ run/all.sh
@@ -3,8 +3,11 @@
 ./run/acdc-locality.sh
 ./run/acdc-object-size.sh
 ./run/acdc-prod-cons.sh
+./acdc-virtual-spans-evaluation.sh
 ./run/threadtest-averaged-for-all-allocators.sh
 ./run/larson-averaged-for-all-allocators.sh
 ./run/shbench-averaged-for-all-allocators.sh
 ./run/active-false-averaged-for-all-allocators.sh
 ./run/passive-false-averaged-for-all-allocators.sh
+./run/scalloc-evaluate-frontend-cleanup.sh
+./run/scalloc-evaluate-span-pool.sh
